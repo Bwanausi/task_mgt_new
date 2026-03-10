a161a51d4a1ca0ca9c55d6b4bff6078f64a4a87c
Bwanausi <fredsansy@gmail.com>
2026-03-10 09:43:12 +0300

    api url change

--- src/api/axios.js
+++ src/api/axios.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8181/api/v1",
+  baseURL: "https://maagizo-tfc.onrender.com/api/v1",
 });
 
 api.interceptors.request.use((config) => {
